19eb64ba6adff6254adedc80b350aa6ed15b72de
Yukun Luo <yukunl@nvidia.com>
2021-03-05 09:28:53 +0800

    Use maven-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -162,6 +162,14 @@ subprojects {
 
     publishing.publications.register("release", MavenPublication::class) {
       from(components["java"])
+      pom {
+        licenses {
+          license {
+            name.set("The Apache License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
+        }
+      }
     }
 
     tasks.bootJar {
